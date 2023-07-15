db950d08a228bb08fc5bc9ceea4cad8f2848a44d
gabbyPassion <129009206+gabbyPassion@users.noreply.github.com>
2023-07-15 13:57:20 -0400

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">Nifty Shop</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
