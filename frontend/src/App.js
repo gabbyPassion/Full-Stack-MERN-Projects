97531cc95d6ae9f21aac246f84109152d2316c97
gabbyPassion <129009206+gabbyPassion@users.noreply.github.com>
2023-07-21 06:50:06 -0400

    Create and Style Product components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <a href="/">Nifty Shop</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="primary" variant="dark">
+            <Container>              
+              <LinkContainer to="/">
+                <Navbar.Brand>The Variety Store</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>       
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved.</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', // 679 x 829
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'High quality Nike shirt'
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'High quality shirt'
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg', // 679 x 829
+            price: 120,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality Nike shirt'
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 120,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'High quality shirt'
+        }
+    ]
+};
+
+export default data;
